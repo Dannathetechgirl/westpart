c96d92df0d8c18d59d86e579f9dc4920cc3a1a53
Grace Kalu <gracekalu@Graces-MacBook-Air.local>
2023-10-24 11:38:07 -0600

    fix: close btn'

--- src/component/Navbar/navbar.jsx
+++ src/component/Navbar/navbar.jsx
@@ -33,7 +33,7 @@ function Navbar() {
                         className='app__hamburger-motion'
                     >
 
-                        <i class="fa-solid fa-xmark" setToggle={false} className='app__HiX'></i>
+                        <i class="fa fa-xmark" setToggle={false} className='app__HiX'></i>
 
                         <ul>
                             {['Home', 'about'].map((item) => (
